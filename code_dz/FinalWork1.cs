9fe85feec14a9f88a24f37f2c2f7f91fcbc7fb5d
Natalya <yara.yarina@mail.ru>
2023-01-11 23:41:00 +0300

    Исправление

--- code_dz/FinalWork1.cs
+++ code_dz/FinalWork1.cs
@@ -3,7 +3,7 @@
 // либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
 // лучше обойтись исключительно массивами.
 
-string[] array1 = new string[5] {"123", "321", "hello", "world", "Russia"};
+string[] array1 = new string[5] {"12345", "67890", "hello", "world", "Russia"};
 string[] array2 = new string[array1.Length];
 int count = 0;
 
@@ -13,5 +13,19 @@ void SecondArrayWithIF(string[] array1, string[] array2)
      for (int i = 0; i < array1.Length; i++)
      {
      if(array1[i].Length <= 3)
-     }
- }
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIF(array1, array2);
+PrintArray(array2);
