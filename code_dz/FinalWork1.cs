bb7cc6a8ecea105eeaf3fb02c8d0c33b46d92a1b
Natalya <yara.yarina@mail.ru>
2023-01-11 23:37:09 +0300

    Первый код

--- code_dz/FinalWork1.cs
+++ code_dz/FinalWork1.cs
@@ -2,3 +2,16 @@
 //длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
 // либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
 // лучше обойтись исключительно массивами.
+
+string[] array1 = new string[5] {"123", "321", "hello", "world", "Russia"};
+string[] array2 = new string[array1.Length];
+int count = 0;
+
+void SecondArrayWithIF(string[] array1, string[] array2)
+ {
+     int count = 0;
+     for (int i = 0; i < array1.Length; i++)
+     {
+     if(array1[i].Length <= 3)
+     }
+ }
